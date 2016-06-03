4d7f70339f3f130802bf2687f195f799294ea28f
veselinka <vesibl@gmail.com>
2016-06-03 19:24:34 +0300

    Task4-for mobile
    not finished

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,16 +5,35 @@ $( document ).ready(function() {
 	// toggle mobile navigation
     $(".menuExpand").click(function(e) {
         $(".hamburger").toggleClass("menuOpen");
+        if (  $('.searching').hasClass('menuOpen') ) {
+            $('.searching').removeClass('menuOpen');
+        }
+         $('.hor-nav>li ul').removeClass('open');
+        e.preventDefault();
+    });
+
+    $('.hor-nav>li span').click(function(e) {
+        $(this).parent().siblings().children('ul').removeClass('open');
+    	$(this).parent().children('ul').toggleClass('open');
+
+    });
+
+
+
+
+    $(".searchPanel").click(function(e) {
+        $(".searching").toggleClass("menuOpen");
+        if (  $('.hamburger').hasClass('menuOpen') ) {
+            $('.hamburger').removeClass('menuOpen');
+        }
         e.preventDefault();
     });
 
 
-    //
-    $('.hor-nav>li span').click(function(e) {
-    	$(this).parent().children('ul').toggleClass('open');
-    });
+
 
 });
+
 
 
 
