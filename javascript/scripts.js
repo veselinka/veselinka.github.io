c43ac11c5fe4f4af17312d87446712ce3f3d65ab
veselinka <vesibl@gmail.com>
2016-06-06 17:46:41 +0300

    task4 -change

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -29,32 +29,9 @@ $( document ).ready(function() {
         }
         e.preventDefault();
     });
+
 });
 
 
 
-// $( document ).ready(function() {
-// console.log(1)
 
-//     $(".subHamburger").click(function(e) {
-    	
-// 		console.log(2)    	
-//         $(".subHamburger").toggleClass("subHamburger");
-//         e.preventDefault();
-//     });
-// });
-
-
-
-
-
-
-
-
-// $('.hor-nav').find('> li').click(function(e) {
-// 	e.preventDefault();
-//     $('.hor-nav > li').not(this).find('ul').slideUp();
-//     $(this).find('ul').stop(true, true).slideToggle(400);
-//     return false;
-
-// })
